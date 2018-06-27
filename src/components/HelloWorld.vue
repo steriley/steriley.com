46740d23edd754873e9fcd688c343be1eea6aa2a
Stephen Riley <ste@steriley.com>
2018-06-27 21:51:36 +0100

    stylelint and stricter linting added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,7 +34,10 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: {
+      type: String,
+      default: 'Hello World',
+    },
   },
 };
 </script>
@@ -44,14 +47,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
