01b52352c4889aea7ea3787cccc817ec542e94ed
Stephen Riley <ste@steriley.com>
2018-06-27 21:21:29 +0100

    add homepage route

--- app/index.js
+++ app/index.js
@@ -6,6 +6,10 @@ const lastfm = require('./lastfm');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+  res.json('hello world');
+});
+
 app.get('/api/twitter', (req, res) => {
   twitter
     .lastTweet('SteRiley', {
