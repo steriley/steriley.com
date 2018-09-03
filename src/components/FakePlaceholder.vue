4d120c049a4a6f9a56d9a68adb50301f69af213a
Stephen Riley <stephen.riley2@jdplc.com>
2018-09-03 16:01:11 +0100

    create fake placeholder component

---
+++ src/components/FakePlaceholder.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="fake-container">
+    <div class="fake fake-image"/>
+    <div class="fake-content">
+      <div class="fake fake-header"/>
+      <div class="fake fake-text"/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fake-container {
+  display: flex;
+}
+
+.fake {
+  opacity: .3;
+  animation-duration: 3s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  animation-name: placeholder-shimmer;
+  animation-timing-function: linear;
+  background-color: #f2f2f2;
+  background-repeat: no-repeat;
+  background-size: 300px 300px;
+  background-image:
+    linear-gradient(
+      to left,
+      #f2f2f2 0%,
+      #ccc 50%,
+      #f2f2f2 100%
+    );
+}
+
+@keyframes placeholder-shimmer {
+  0% {
+    background-position: -468px 0;
+  }
+
+  100% {
+    background-position: 468px 0;
+  }
+}
+
+.fake-content {
+  width: 100%;
+}
+
+$size: 4rem;
+$margin: .5rem;
+
+.fake-image {
+  flex-shrink: 0;
+  height: $size;
+  margin-right: $margin;
+  width: $size;
+}
+
+.fake-header {
+  height: ($size - $margin) / 3;
+  margin-bottom: $margin;
+}
+
+.fake-text {
+  height: ($size - $margin) / 1.5;
+  width: 100%;
+}
+</style>
