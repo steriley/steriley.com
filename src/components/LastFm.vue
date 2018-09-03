71ec0cf9816e1c14bb4e0a6ff60bf4b05feb509b
Stephen Riley <stephen.riley2@jdplc.com>
2018-09-03 16:01:33 +0100

    add fake placeholder to last fm

--- src/components/LastFm.vue
+++ src/components/LastFm.vue
@@ -1,27 +1,25 @@
 <template>
   <ol class="last-fm">
     <li v-for="(track, key) in tracks" :key="key" class="last-fm__track">
-      <a :href="track.url" class="track">
-        <div class="cover">
-          <img :src="(track.artwork !== '') ? track.artwork : '/img/no-cover-art.png'" :alt="`${track.artist} - ${track.title}`" class="track__cover">
-        </div>
-        <div class="details">
-          <span class="track__artist">{{ track.artist }}</span>
-          <span class="track__title">{{ track.title }}</span>
-          <span :date-time="track.date.utc" :class="{'track__date--now' : track.date.utc === 0}"
-                class="track__date">{{ track.date.formatted }}</span>
-          <button class="track__video" @click.prevent="loadVideo(track.artist, track.title)">
-            Watch Video
-          </button>
-        </div>
-      </a>
+      <LastFmTrack v-if="track" :track="track"/>
+      <FakePlaceholder v-else/>
     </li>
   </ol>
 </template>
 
 <script>
+import FakePlaceholder from '@/components/FakePlaceholder.vue';
+import LastFmTrack from '@/components/LastFmTrack.vue';
+
+const tracksToDisplay = 6;
+
 export default {
   name: 'LastFm',
+
+  components: {
+    FakePlaceholder,
+    LastFmTrack,
+  },
 
   props: {
     fetch: {
@@ -30,15 +28,13 @@ export default {
     },
   },
 
-  data() {
-    return {
-      tracks: [],
-      displayVideo: false,
-    };
-  },
+  data: () => ({
+    displayVideo: false,
+    tracks: new Array(tracksToDisplay).fill(0),
+  }),
 
   mounted() {
-    this.fetch('lastfm/6')
+    this.fetch(`lastfm/${tracksToDisplay}`)
       .then((json) => {
         this.tracks = json;
       });
@@ -63,61 +59,5 @@ export default {
 .last-fm__track {
   margin: 0 0 1em 0;
   position: relative;
-}
-
-.track {
-  display: flex;
-  min-height: 64px;
-  color: #000;
-  text-decoration: none;
-
-  span {
-    display: block;
-  }
-}
-
-.details {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  width: 100%;
-  padding-bottom: 1rem;
-}
-
-.track__cover {
-  float: left;
-  margin-right: 5px;
-}
-
-.track__artist {
-  font-weight: 700;
-}
-
-.track__date {
-  bottom: 0;
-  font-size: .9em;
-  position: absolute;
-  right: 0;
-}
-
-.track__date--now {
-  background: url('/img/icon_eq.gif') no-repeat 0 50%;
-  padding-left: 1rem;
-}
-
-.track__video {
-  align-self: flex-start;
-  background-color: transparent;
-  border: 0;
-  bottom: 0;
-  color: #fff;
-  font-size: .9em;
-  height: auto;
-  left: 0;
-  outline: none;
-  padding: 0;
-  position: absolute;
-  text-transform: lowercase;
-  z-index: 10;
 }
 </style>
---
+++ src/components/LastFmTrack.vue
@@ -0,0 +1,96 @@
+<template>
+  <a :href="track.url" class="track">
+    <div class="cover">
+      <img
+        :src="(track.artwork !== '') ? track.artwork : '/img/no-cover-art.png'"
+        :alt="`${track.artist} - ${track.title}`"
+        class="track__cover"
+      >
+    </div>
+    <div class="details">
+      <span class="track__artist">{{ track.artist }}</span>
+      <span class="track__title">{{ track.title }}</span>
+      <span :date-time="track.date.utc" :class="{'track__date--now' : track.date.utc === 0}"
+            class="track__date">{{ track.date.formatted }}</span>
+      <button class="track__video" @click.prevent="loadVideo(track.artist, track.title)">
+        Watch Video
+      </button>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    track: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  methods: {
+    loadVideo(artist, title) {
+      this.displayVideo = `${artist} ${title}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.track {
+  display: flex;
+  min-height: 64px;
+  color: #000;
+  text-decoration: none;
+
+  span {
+    display: block;
+  }
+}
+
+.details {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  padding-bottom: 1rem;
+}
+
+.track__cover {
+  float: left;
+  margin-right: 5px;
+}
+
+.track__artist {
+  font-weight: 700;
+}
+
+.track__date {
+  bottom: 0;
+  font-size: .9em;
+  position: absolute;
+  right: 0;
+}
+
+.track__date--now {
+  background: url('/img/icon_eq.gif') no-repeat 0 50%;
+  padding-left: 1rem;
+}
+
+.track__video {
+  align-self: flex-start;
+  background-color: transparent;
+  border: 0;
+  bottom: 0;
+  color: #fff;
+  font-size: .9em;
+  height: auto;
+  left: 0;
+  outline: none;
+  padding: 0;
+  position: absolute;
+  text-transform: lowercase;
+  z-index: 10;
+}
+</style>
+
