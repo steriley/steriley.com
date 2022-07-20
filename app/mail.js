794e3cb612c7d8671e182df7661120d8ff553c33
Ste Riley <ste@steriley.com>
2022-07-20 19:06:48 +0100

    refactor: change how messages are sent

--- app/mail.js
+++ app/mail.js
@@ -1,29 +1,30 @@
 const sgMail = require('@sendgrid/mail');
 
-const API_KEY = process.env.SENDGRID_API_KEY;
-
-sgMail.setApiKey(API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports.send = obj =>
   new Promise((resolve, reject) => {
+    const text = `${obj.comments}
+      \n---------------------------------------------
+      \nMail sent by: ${obj.name}: ${obj.email} from steriley.com
+    `;
+
+    const msg = {
+      to: process.env.SENDGRID_EMAIL,
+      from: process.env.SENDGRID_EMAIL,
+      subject: obj.subject,
+      text,
+      html: `<pre style="font-family:Arial;font-size:16px">${text}</pre>`,
+    };
+
     const { timestamp } = obj;
     const now = +new Date();
     const validTime = now - timestamp > 10000;
     const success = { message: 'Your message has been sent!' };
-    let text = obj.comments;
-
-    text += `\n\n---------------------------------------------\n`;
-    text += `Mail sent by: ${obj.name} <${obj.email}> from steriley.com`;
 
     if (validTime && obj.message === '') {
       return sgMail
-        .send({
-          to: process.env.SENDGRID_EMAIL,
-          from: `${obj.name} <${obj.email}>`,
-          subject: obj.subject,
-          text,
-          html: `<pre>${text}<pre>`,
-        })
+        .send(msg)
         .then(() => resolve(success))
         .catch(error => reject(error));
     }
