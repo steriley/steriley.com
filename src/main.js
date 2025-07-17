16d84dc9d5b0d2853d7f9e59459cec2d615b4114
Ste Riley <ste@steriley.com>
2025-07-17 23:10:55 +0100

    chore: get project building again

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 import { createApp } from 'vue';
 import VueLazyload from 'vue-lazyload';
-import * as Sentry from '@sentry/browser';
-import { BrowserTracing } from "@sentry/tracing";
+// import * as Sentry from '@sentry/browser';
+// import { BrowserTracing } from "@sentry/tracing";
 
 import App from './App.vue';
 
 const app = createApp(App);
 
-Sentry.init({
-  app,
-  dsn: "https://7237bfe60af54442a795f5f706b1e469@o185486.ingest.sentry.io/1279887",
-  integrations: [
-    new BrowserTracing({
-      tracingOrigins: ["localhost", "steriley.com", /^\//],
-    }),
-  ],
-  tracesSampleRate: 0.25,
-});
+// Sentry.init({
+//   app,
+//   dsn: "https://7237bfe60af54442a795f5f706b1e469@o185486.ingest.sentry.io/1279887",
+//   integrations: [
+//     new BrowserTracing({
+//       tracingOrigins: ["localhost", "steriley.com", /^\//],
+//     }),
+//   ],
+//   tracesSampleRate: 0.25,
+// });
 
 app.use(VueLazyload);
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-      },
-      '/.netlify/': {
-        target: 'http://localhost:9000',
-        pathRewrite: { '^/.netlify/functions': '' },
-      },
+      '/api': 'http://localhost:3000',
     },
   },
   test: {
