23b52215acfcf5aa0b736f0c86e6d5907557d32c
Ste Riley <ste@steriley.com>
2025-07-21 21:40:43 +0100

    fix: sentry logging

--- src/main.js
+++ src/main.js
@@ -1,22 +1,18 @@
 import { createApp } from 'vue';
 import VueLazyload from 'vue-lazyload';
-// import * as Sentry from '@sentry/browser';
-// import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from '@sentry/vue';
 
 import App from './App.vue';
 
 const app = createApp(App);
 
-// Sentry.init({
-//   app,
-//   dsn: "https://7237bfe60af54442a795f5f706b1e469@o185486.ingest.sentry.io/1279887",
-//   integrations: [
-//     new BrowserTracing({
-//       tracingOrigins: ["localhost", "steriley.com", /^\//],
-//     }),
-//   ],
-//   tracesSampleRate: 0.25,
-// });
+Sentry.init({
+  app,
+  dsn: 'https://7237bfe60af54442a795f5f706b1e469@o185486.ingest.sentry.io/1279887',
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.25,
+  tracePropagationTargets: ['localhost', /^https:\/\/steriley\.com\/api/],
+});
 
 app.use(VueLazyload);
 app.mount('#app');
