8734c2e9b6b7b077054955efbf9440c18a64c650
steriley <ste@steriley.com>
2026-01-20 21:55:36 +0000

    fix: display of no cover art in tracks

--- src/components/LastFmTrack.vue
+++ src/components/LastFmTrack.vue
@@ -17,11 +17,13 @@ defineProps<{
 </script>
 
 <template>
-  <div class="relative h-24 w-24 shrink-0 overflow-hidden rounded-lg md:h-auto md:w-full md:aspect-square">
+  <div
+    class="relative h-24 w-24 shrink-0 overflow-hidden rounded-lg md:h-auto md:w-full md:aspect-square"
+  >
     <img
       :alt="`${artist} - ${title}`"
       class="h-full w-full object-cover shadow-md"
-      :src="artwork !== '' ? artwork : '/img/no-cover-art.png'"
+      :src="artwork !== '' ? artwork : '/no-cover-art.jpg'"
     />
     <a
       :href="url"
@@ -33,16 +35,18 @@ defineProps<{
     </a>
   </div>
   <div class="min-w-0 flex-1 overflow-hidden md:w-full">
-  <h4
-    class="truncate text-left text-sm font-bold text-primary group-hover:text-secondary md:mt-3 md:text-center"
-  >
-    {{ title }}
-  </h4>
-  <p class="truncate text-left text-xs text-secondary md:text-center">{{ artist }}</p>
-  <span
-    :date-time="date.utc"
-    class="mt-1 block truncate text-left text-[10px] text-secondary/70 md:text-center"
-    >{{ date.formatted }}</span
-  >
+    <h4
+      class="truncate text-left text-sm font-bold text-primary group-hover:text-secondary md:mt-3 md:text-center"
+    >
+      {{ title }}
+    </h4>
+    <p class="truncate text-left text-xs text-secondary md:text-center">
+      {{ artist }}
+    </p>
+    <span
+      :date-time="date.utc"
+      class="mt-1 block truncate text-left text-[10px] text-secondary/70 md:text-center"
+      >{{ date.formatted }}</span
+    >
   </div>
 </template>
